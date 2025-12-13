33f9ecf1941be7af4517b9123199443c3fc6c061
Luigi Araujo <marioverdeplayy@gmail.com>
2025-12-13 19:10:20 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,137 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+void main() {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Digite o valor em Hexadecimal: ");
+    String decima_H = input.next();
+
+    System.out.print("Qual conversão você quer fazer, digite (B) para binario e (D) para decimal: ");
+    char operacao = input.next().charAt(0);
+
+
+
+    List<Integer> binario = new ArrayList<>();
+    for (int i = 0; i < decima_H.length(); i++) {
+        switch (decima_H.toLowerCase().charAt(i)) {
+            case '0':
+                binario.add(0);
+                binario.add(0);
+                binario.add(0);
+                binario.add(0);
+                break;
+            case '1':
+                binario.add(0);
+                binario.add(0);
+                binario.add(0);
+                binario.add(1);
+                break;
+            case '2':
+                binario.add(0);
+                binario.add(0);
+                binario.add(1);
+                binario.add(0);
+                break;
+            case '3':
+                binario.add(0);
+                binario.add(0);
+                binario.add(1);
+                binario.add(1);
+                break;
+            case '4':
+                binario.add(0);
+                binario.add(1);
+                binario.add(0);
+                binario.add(0);
+                break;
+            case '5':
+                binario.add(0);
+                binario.add(1);
+                binario.add(0);
+                binario.add(1);
+                break;
+            case '6':
+                binario.add(0);
+                binario.add(1);
+                binario.add(1);
+                binario.add(0);
+                break;
+            case '7':
+                binario.add(0);
+                binario.add(1);
+                binario.add(1);
+                binario.add(1);
+                break;
+            case '8':
+                binario.add(1);
+                binario.add(0);
+                binario.add(0);
+                binario.add(0);
+                break;
+            case '9':
+                binario.add(1);
+                binario.add(0);
+                binario.add(0);
+                binario.add(1);
+                break;
+            case 'a':
+                binario.add(1);
+                binario.add(0);
+                binario.add(1);
+                binario.add(0);
+                break;
+            case 'b':
+                binario.add(1);
+                binario.add(0);
+                binario.add(1);
+                binario.add(1);
+                break;
+            case 'c':
+                binario.add(1);
+                binario.add(1);
+                binario.add(0);
+                binario.add(0);
+                break;
+            case 'd':
+                binario.add(1);
+                binario.add(1);
+                binario.add(0);
+                binario.add(1);
+                break;
+            case 'e':
+                binario.add(1);
+                binario.add(1);
+                binario.add(1);
+                binario.add(0);
+                break;
+            case 'f':
+                binario.add(1);
+                binario.add(1);
+                binario.add(1);
+                binario.add(1);
+                break;
+        }
+
+    }
+    if (operacao == 'B') {
+        System.out.print("O Hexadecimal " + decima_H + " para binario : ");
+
+        for (int i = 0; i < binario.toArray().length; i++) {
+            System.out.print(binario.get(i));
+        }
+    } else if (operacao == 'D'){
+
+        int soma = 0;
+        int ex = 0;
+        for (int i = binario.size() - 1; i >= 0; i--) {
+            soma += binario.get(i) * Math.pow(2, ex);
+            ex++;
+        }
+        System.out.print("O Hexadecimal " + decima_H + " para decimal: " + soma);
+
+    }
+
+}
+
