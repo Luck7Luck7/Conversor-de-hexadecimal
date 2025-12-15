f68e352a5899147c8f266542d01217df7653d192
Luigi <124471943+Luck7Luck7@users.noreply.github.com>
2025-12-15 14:06:44 -0300

    conversor de hexa para binario movido para arquivo binario

--- src/converter/Binario.java
+++ src/converter/Binario.java
@@ -1,5 +1,6 @@
 package converter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Binario {
@@ -12,5 +13,111 @@ public class Binario {
             ex++;
         }
         return soma;
+    }
+
+    public List<Integer> hexadecimal_Binario(String hexadecimal) {
+        List<Integer> binario = new ArrayList<>();
+        for (int i = 0; i < hexadecimal.length(); i++) {
+            switch (hexadecimal.toLowerCase().charAt(i)) {
+                case '0':
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(0);
+                    break;
+                case '1':
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(1);
+                    break;
+                case '2':
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(0);
+                    break;
+                case '3':
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(1);
+                    break;
+                case '4':
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(0);
+                    break;
+                case '5':
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(1);
+                    break;
+                case '6':
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(0);
+                    break;
+                case '7':
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(1);
+                    break;
+                case '8':
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(0);
+                    break;
+                case '9':
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(0);
+                    binario.add(1);
+                    break;
+                case 'a':
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(0);
+                    break;
+                case 'b':
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(1);
+                    binario.add(1);
+                    break;
+                case 'c':
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(0);
+                    break;
+                case 'd':
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(0);
+                    binario.add(1);
+                    break;
+                case 'e':
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(0);
+                    break;
+                case 'f':
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(1);
+                    binario.add(1);
+                    break;
+            }
+
+        }
+        return binario;
     }
 }
